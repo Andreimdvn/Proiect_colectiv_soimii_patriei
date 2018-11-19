07c61178bfd5645d1ddf66fc0a87cd7198472cf3
andamoga <andamoga23@gmail.com>
2018-11-19 16:45:08 +0200

    Added tab for seeing other clients

--- Frontend/client-app/src/App.tsx
+++ Frontend/client-app/src/App.tsx
@@ -2,6 +2,7 @@ import TabMenu from "./components/tab-menu/TabMenu";
 import rootStore from "./store/root-store";
 import HomeRoute from "./components/home";
 import AddJobRoute from "./components/jobs/add-job-form";
+import ClientListRoute from "./components/clients/client-list";
 import "./App.css";
 
 import { Provider } from "mobx-react";
@@ -21,6 +22,7 @@ class App extends React.Component {
                 <TabMenu viewStore={rootStore.viewStore} />
                 <HomeRoute />
                 <AddJobRoute />
+                <ClientListRoute />
               </React.Fragment>
             </Router>
           </React.Fragment>
