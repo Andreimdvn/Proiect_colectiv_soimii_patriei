d567137cc92c9898a8329ac947caa9ea88e6d370
Alin Oancea <oanceaalinandrei@gmail.com>
2018-11-19 08:56:34 +0200

    Added backend support for register query; some modifications to orm structure

--- Backend/Communication/flask_server.py
+++ Backend/Communication/flask_server.py
@@ -1,10 +1,23 @@
 import json
 import logging
+import re
 
 from flask import Flask, request
 from flask_socketio import SocketIO
 
 from Controller.controller import Controller
+
+
+register_fields = {
+    'username': str,
+    'password': str,
+    'email': lambda x: re.match(r'(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)', x),
+    'first_name': str,
+    'last_name': str,
+    'date_of_birth': lambda x: re.match(r'^[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}$', x),
+    'phone': str,
+    'account_type': ('client', 'provider')
+}
 
 
 class FlaskServer:
@@ -37,9 +50,45 @@ class FlaskServer:
         self.shutdown_server()
 
     def init_requests(self):
-        self.flask_app.add_url_rule("/test", "test_request", self.test_request, methods=["GET", "POST"])
+        self.flask_app.add_url_rule('/test', 'test_request', self.test_request, methods=['GET', 'POST'])
+        self.flask_app.add_url_rule('/api/register', 'register', self.register, methods=['POST'])
 
     def test_request(self):
         self.request_data = request.get_json()
         self.logger.debug("Req data: {}".format(self.request_data))
         return json.dumps("Hello world! Got req: {}".format(self.request_data))
+
+    def register(self):
+        request_data = request.get_json()
+        sanitized_request = {}
+        status = -1
+        response = None
+
+        for k, v in register_fields.items():
+            if k not in request_data:
+                if not response:
+                    response = '[!] Field(s) [%s' % (k,)
+                else:
+                    response += ', ' + k
+            elif type(v) in (str,):
+                sanitized_request[k] = v(request_data[k])
+            elif type(v) in (tuple,):
+                if request_data[k] not in v:
+                    if not response:
+                        response = '[!] Field [%s] has an invalid value!' % (k,)
+                        break
+                else:
+                    sanitized_request[k] = request_data[k]
+            else:
+                if not v(request_data[k]):
+                    if not response:
+                        response = '[!] Field [%s] has an invalid value!' % (k,)
+                        break
+                else:
+                    sanitized_request[k] = request_data[k]
+        else:
+            if response:
+                response += '] is(are) mandatory!'
+        if not response:
+            status, response = self.controller.register(**sanitized_request)
+        return json.dumps({'status': status, 'response': response})
--- Backend/Controller/controller.py
+++ Backend/Controller/controller.py
@@ -1,3 +1,13 @@
+from Repository.repo_jobs import RepositoryJobs
+
+
 class Controller:
+
     def __init__(self, db_config):
+        self.repo = RepositoryJobs(db_config)
+
+    def login(self, username=None, password=None):
         pass
+
+    def register(self, **args):
+        return self.repo.register(**args)
