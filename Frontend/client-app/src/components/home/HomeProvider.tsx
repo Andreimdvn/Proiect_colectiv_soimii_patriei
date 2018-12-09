f5ce5e9e619bad694de7195f8ee0628f448fc474
ciprianmunteanu <justashadownfg@gmail.com>
2018-12-10 01:15:01 +0200

    Still pretty ugly recommended jobs table

--- Frontend/client-app/src/components/home/HomeProvider.tsx
+++ Frontend/client-app/src/components/home/HomeProvider.tsx
@@ -4,6 +4,7 @@ import { inject, observer } from "mobx-react";
 import { ViewStore } from "src/store/view-store";
 import {RecommendedJobsTable} from "../provider/recommended-jobs/RecommendedJobsTable";
 import {Job} from "../../view-models/Job";
+import Paper from "@material-ui/core/Paper/Paper";
 
 interface Props {
   viewStore: ViewStore;
@@ -18,15 +19,17 @@ export class HomeProvider extends React.Component<Props> {
 
   render(): React.ReactNode {
     return (
-        <React.Fragment>
+      <React.Fragment>
+        <Paper className="paper">
           <div>Recommended for you:</div><br/>
           <RecommendedJobsTable jobs={[
-            new Job("1", "cleaning", "clean my house", "you dad", "dinner", "20-09-2018"),
-            new Job("2", "dog walk", "walk good boie", "you mom", "5", "23-09-2018"),
-            new Job("3", "kid", "take care of my toddler", "you sister", "12", "30-09-2018"),
+            new Job("1", "cleaning", "clean my house", "your dad", "dinner", "20-09-2018"),
+            new Job("2", "dog walk", "walk good boie", "your mom", "5", "23-09-2018"),
+            new Job("3", "kid", "take care of my toddler", "your sister", "12", "30-09-2018"),
           ]}/>
-          <div>*history*</div><br/>
-        </React.Fragment>
+        </Paper>
+        <div>*history*</div><br/>
+      </React.Fragment>
     );
   }
 }
