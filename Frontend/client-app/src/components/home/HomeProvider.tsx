01bced5dea0be856935500b9b0ef449f65c1455e
Bogdan Florea <bogdan.florea@evozon.com>
2018-12-10 15:47:53 +0200

    Figured out material ui theme injection

--- Frontend/client-app/src/components/home/HomeProvider.tsx
+++ Frontend/client-app/src/components/home/HomeProvider.tsx
@@ -5,6 +5,7 @@ import { ViewStore } from "src/store/view-store";
 import {RecommendedJobsTable} from "../provider/recommended-jobs/RecommendedJobsTable";
 import {Job} from "../../view-models/Job";
 import Paper from "@material-ui/core/Paper/Paper";
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 
 interface Props {
   viewStore: ViewStore;
@@ -18,15 +19,23 @@ export class HomeProvider extends React.Component<Props> {
   }
 
   render(): React.ReactNode {
+    const theme = createMuiTheme({
+      palette: {
+        type: 'dark',
+      },
+    });
+
     return (
       <React.Fragment>
         <Paper className="paper">
           <div>Recommended for you:</div><br/>
-          <RecommendedJobsTable jobs={[
-            new Job("1", "cleaning", "clean my house", "your dad", "dinner", "20-09-2018"),
-            new Job("2", "dog walk", "walk good boie", "your mom", "5", "23-09-2018"),
-            new Job("3", "kid", "take care of my toddler", "your sister", "12", "30-09-2018"),
-          ]}/>
+          <MuiThemeProvider theme = {theme}>
+            <RecommendedJobsTable jobs={[
+              new Job("1", "cleaning", "clean my house", "bob321", "dinner", "20-09-2018"),
+              new Job("2", "dog walk", "walk good boie", "angry_cat98", "5", "23-09-2018"),
+              new Job("3", "kid", "take care of my toddler!!", "xfg45", "12", "30-09-2018"),
+            ]}/>
+          </MuiThemeProvider>
         </Paper>
         <div>*history*</div><br/>
       </React.Fragment>
--- Frontend/client-app/src/components/provider/recommended-jobs/RecommendedJobsTable.tsx
+++ Frontend/client-app/src/components/provider/recommended-jobs/RecommendedJobsTable.tsx
@@ -1,42 +1,83 @@
 import * as React from "react";
 import {Job} from "../../../view-models/Job";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import { withStyles, Theme } from '@material-ui/core/styles';
+import { WithStyles, createStyles } from '@material-ui/core';
 
-interface Props {
+const JobsTableCell = withStyles(theme => ({
+  head: {
+    backgroundColor: theme.palette.background,
+    color: theme.palette.text,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const styles = (theme: Theme) => createStyles({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+    backgroundColor: theme.palette.background.default
+  },
+  row: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.background.default,
+    },
+  },
+});
+
+interface Props extends WithStyles<typeof styles>{
   jobs:Job[]
 }
 
-export class RecommendedJobsTable extends React.Component<Props> {
-  constructor(props) {
-    super(props);
+export const RecommendedJobsTable = withStyles(styles)(
+  class RecommendedJobsTableBase extends React.Component<Props> {
+    constructor(props) {
+      super(props);
+    }
 
+    render() {
+      const {classes} = this.props;
+
+      return (
+        <React.Fragment>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <JobsTableCell>Type</JobsTableCell>
+                <JobsTableCell>Description</JobsTableCell>
+                <JobsTableCell>Reward</JobsTableCell>
+                <JobsTableCell>Publisher</JobsTableCell>
+                <JobsTableCell>Date</JobsTableCell>
+              </TableRow>
+            </TableHead>
+
+            <TableBody>
+              {this.props.jobs.map(job =>{
+                return (
+                  <TableRow key={job.id}>
+                    <JobsTableCell>{job.type}</JobsTableCell>
+                    <JobsTableCell>{job.descriptionPreview}</JobsTableCell>
+                    <JobsTableCell>{job.reward}</JobsTableCell>
+                    <JobsTableCell>{job.publisher}</JobsTableCell>
+                    <JobsTableCell>{job.date}</JobsTableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+
+          </Table>
+        </React.Fragment>
+      );
+    }
   }
-
-  render() {
-    return (
-      <React.Fragment>
-        <table>
-          <tr>
-            <th>Type</th>
-            <th>Description</th>
-            <th>Reward</th>
-            <th>Publisher</th>
-            <th>Date</th>
-          </tr>
-
-          {this.props.jobs.map(job =>{
-            return (
-              <tr key={job.id}>
-                <td>{job.type}</td>
-                <td>{job.descriptionPreview}</td>
-                <td>{job.reward}</td>
-                <td>{job.publisher}</td>
-                <td>{job.date}</td>
-              </tr>
-            );
-          })}
-
-        </table>
-      </React.Fragment>
-    );
-  }
-}
+);
