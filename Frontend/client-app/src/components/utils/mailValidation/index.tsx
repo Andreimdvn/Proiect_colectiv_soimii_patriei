d7c9eb11bf727179cedf3a9d687363f33366ecc1
andamoga <andamoga23@gmail.com>
2019-01-18 14:28:01 +0200

    Fixed redirection without MailValidation

--- Frontend/client-app/src/components/utils/mailValidation/index.tsx
+++ Frontend/client-app/src/components/utils/mailValidation/index.tsx
@@ -1,11 +1,11 @@
 import { Route } from "react-router";
 import * as React from "react";
-import {MailValidation} from "./MailValidation";
+import { MailValidation } from "./MailValidation";
 
 export default () => {
-    return (
-        <React.Fragment>
-            <Route path="/register" component={Register} />
+  return (
+    <React.Fragment>
+      <Route path="/validation" component={MailValidation} />
     </React.Fragment>
-);
+  );
 };
